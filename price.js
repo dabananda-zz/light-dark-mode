3a703a6e321dd52947d39345c59c702a6990115d
Dabananda Mitra <dmitraofficial@gmail.com>
2021-09-10 21:27:13 +0600

    Full webpage

---
+++ price.js
@@ -0,0 +1,60 @@
+const amountEl = document.getElementById('amount');
+const addEl = document.getElementById('add');
+const deleteEl = document.getElementById('delete');
+const totalPriceEl = document.getElementById('total-price');
+
+// Price and item quantity set to zero on change of select tag options
+function onSelectChange() {
+    amountEl.innerText = 0;
+    totalPriceEl.innerText = '00.00';
+}
+
+// Count and set total price
+function totalPriceCount(value) {
+    const selectEl = document.getElementById('select');
+    const selectValue = parseInt(selectEl.options[select.selectedIndex].value);
+    let totalPrice = 0;
+
+    switch (selectValue) {
+        case 1:
+            totalPrice = 3.99 * value;
+            break;
+        case 2:
+            totalPrice = 4.99 * value;
+            break;
+        case 3:
+            totalPrice = 1.99 * value;
+            break;
+        case 4:
+            totalPrice = 1.29 * value;
+            break;
+        default:
+            totalPrice = 1.49 * value;
+            break;
+    }
+
+    totalPriceEl.innerText = totalPrice;
+}
+
+// Functionality of increase button (+)
+function increaseAmount() {
+    let value = parseInt(amountEl.innerText);
+    value += 1;
+    amountEl.innerText = value;
+    totalPriceCount(value);
+}
+
+// Functionality of decrease button (-)
+function decreaseAmount() {
+    let value = parseInt(amountEl.innerText);
+    value -= 1;
+    if (value < 1) {
+        alert('Food item can not be 0');
+    } else {
+        amountEl.innerText = value;
+    }
+    totalPriceCount(value);
+}
+
+addEl.addEventListener('click', increaseAmount);
+deleteEl.addEventListener('click', decreaseAmount);
--- theme.js
+++ theme.js
@@ -3,7 +3,10 @@ const iconEl = document.getElementById('icon');
 function toggleTheme(isTrue) {
     isTrue ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode');
     isTrue ? localStorage.setItem('theme', 'dark') : localStorage.setItem('theme', 'light');
-    isTrue ? iconEl.classList.replace('bi-brightness-high-fill', 'bi-moon-stars-fill') : iconEl.classList.replace('bi-moon-stars-fill', 'bi-brightness-high-fill');
+    isTrue
+        ? iconEl.classList.replace('bi-brightness-high-fill', 'bi-moon-stars-fill')
+        : iconEl.classList.replace('bi-moon-stars-fill', 'bi-brightness-high-fill');
+    isTrue ? document.getElementById('table').classList.remove('table-striped') : document.getElementById('table').classList.add('table-striped');
 }
 
 iconEl.addEventListener('click', function () {
