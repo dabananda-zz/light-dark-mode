9fa377a984e1ebdc6ecc3344a7f6e29177676264
Dabananda Mitra <dmitraofficial@gmail.com>
2021-09-10 10:50:31 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const iconEl = document.getElementById('icon');
+
+function toggleTheme(isTrue) {
+    isTrue ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode');
+    isTrue ? localStorage.setItem('theme', 'dark') : localStorage.setItem('theme', 'light');
+    isTrue ? iconEl.classList.replace('bi-brightness-high-fill', 'bi-moon-stars-fill') : iconEl.classList.replace('bi-moon-stars-fill', 'bi-brightness-high-fill');
+}
+
+iconEl.addEventListener('click', function () {
+    currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme) {
+        if (currentTheme == 'light') {
+            toggleTheme(true);
+        } else {
+            toggleTheme(false);
+        }
+    }
+});
+
+let currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    if (currentTheme == 'light') {
+        toggleTheme(false);
+    } else {
+        toggleTheme(true);
+    }
+} else {
+    toggleTheme(true);
+}
